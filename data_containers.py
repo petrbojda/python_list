d8e487acef7748158dd237082efad1b408e0758c
petrbojda <petr.bojda@email.cz>
2017-11-29 12:20:33 +0100

    framework created

--- data_containers.py
+++ data_containers.py
@@ -5,21 +5,17 @@ import configparser
 import argparse
 import itertools
 import copy
-
+import csv
 
 class DetectionPoint(object):
-    def __init__(self, mcc=0, beam=0, rng=0.0,
-                 vel=0.0, az=0.0):
-
-        self._y_correction_dir = -1 if left else 1
+    def __init__(self, mcc=0, x=0.0, y=0.0,
+                 vel=0.0, az=0.0, rng=0.0):
         self._mcc = mcc
-        self._beam = beam
-        self._rng = rng
         self._vel = vel
+        self._x = x
+        self._y = y
         self._az = az
-        self._x = self._rng * np.cos(self._az)
-        self._y = self._y_correction_dir * (self._rng * np.sin(self._az) + car_width / 2)
-
+        self._rng = rng
 
 class DetectionList(list):
     def __init__(self):
@@ -31,19 +27,38 @@ class DetectionList(list):
         self._rng_minmax = (0, 0)
         self._mcc_minmax = (0, 0)
 
-    def __iter__(self):
-        return
-
-
     def append_detection(self, detection_point):
         self.append(detection_point)
-        self._y_minmax = (min([elem._y for elem in self]), max([elem._y for elem in self]))
-        self._x_minmax = (min([elem._x for elem in self]), max([elem._x for elem in self]))
+        self._y_minmax = (min([elem._x for elem in self]), max([elem._x for elem in self]))
+        self._x_minmax = (min([elem._y for elem in self]), max([elem._y for elem in self]))
         self._az_minmax = (min([elem._az for elem in self]), max([elem._az for elem in self]))
         self._vel_minmax = (min([elem._vel for elem in self]), max([elem._vel for elem in self]))
         self._rng_minmax = (min([elem._rng for elem in self]), max([elem._rng for elem in self]))
         self._mcc_minmax = (min([elem._mcc for elem in self]), max([elem._mcc for elem in self]))
 
     def append_data_from_csv(self,filename):
+        with open (filename,newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print("dc: Appending data from a csv file: mcc",row['mcc'],"x",row['x'],"y",row['y'],"vel",row['vel'])
+                self.append_detection(DetectionPoint(mcc=int(row['mcc']),
+                                                     x=float(row['x']),
+                                                     y=float(row['y']),
+                                                     vel=float(row['vel']) ))
         data = None
         return data
+
+    def get_min_mcc(self):
+        return self._mcc_minmax[0]
+
+    def get_max_mcc(self):
+        return self._mcc_minmax[1]
+
+def cnf_file_read(cnf_file):
+        # Read the configuration file
+        config = configparser.ConfigParser()
+        config.read(cnf_file)  # "./setup.cnf"
+
+        # Get a path to a folder with data
+        conf_data = {"datafile": config.get('Datasets', 'data_01')}
+        return conf_data
--- filtering.py
+++ filtering.py
@@ -6,72 +6,25 @@ import numpy as np
 
 
 def main(config_data):
-    selection = {"beam_tp": config_data["beams_tp"],
-                 "mcc_tp": None, "x_tp": None, "y_tp": None,
-                 "rng_tp": None, "vel_tp": (-5, 5), "az_tp": None,
-                 "trackID_tp": None, }
+    selection = {"mcc_tp": None, "x_tp": None, "y_tp": None,
+                 "rng_tp": None, "vel_tp": (-5, 5), "az_tp": None}
     # Structure 'selection' constrains  data to use as input to the tracker.
     # An exact value or interval of 'mcc', 'azimuth', 'range' ... etc can be
     # specified here to block unwanted data to enter.
 
 
-    # Load Data from .mat files
-    if config_data["filename"]:
-        l = []
-        l.append(config_data["path_data_folder"])
-        l.append(config_data["filename"])
-        leftradar_path = ''.join(l)
+    # Load Data from .csv file
+    if config_data["datafile"]:
+        data_path = config_data["datafile"]
 
         lst_det = dc.DetectionList()
-        lst_det.append_data_from_csv(leftradar_path,)
+        lst_det.append_data_from_csv(data_path)
 
-        print("filter_framework: MCC Left starts at: ", mcc_interval_LR[0],
-              "and ends at: ", mcc_interval_LR[1])
+        print("filtering: MCCs start at: ", lst_det.get_min_mcc(),
+              "and end at: ", lst_det.get_max_mcc())
 
-    # Calculate valid mcc interval for detections to be presented
-    if config_data["filename"]:
-        mcc_start = mcc_interval_LR[0]
-        mcc_end = mcc_interval_LR[1]
-
-    mcc_end = mcc_start + 1000
-    print("filter_framework: MCC starts at: ", mcc_start, "MCC ends at: ", mcc_end)
-    mcc_step = 1
-
-    #----------------- Filtering loop
-    i_prev = mcc_start
-    for i in range(mcc_start, mcc_end, mcc_step):  # number of frames
-
-        selection["mcc_tp"] = (i_prev, i)
-        #-------------- Left radar filter
-        if lst_det_LR:
-            lst_det_per_loop_cycle_LR = lst_det_LR.get_lst_detections_selected(selection=selection)
-
-            # TODO: Is it correct to assign this for every iteration? Potential to write
-            # more effective code.
-            if lst_det_per_loop_cycle_LR:
-                print('filter_framework: Number of detections for a LR mcc ', i, 'is: ',
-                      len(lst_det_per_loop_cycle_LR))
-                track_mgmt_LR.new_detections(lst_det_per_loop_cycle_LR)
-                lst_not_assigned_LR,new_track_LR = track_mgmt_LR.port_data("track_init")
-                if new_track_LR:
-                    print("filter_framework: Type of ported new_track list",type(new_track_LR))
-                    print("filter_framework: Type of ported new_track element", type(new_track_LR[-1]))
-                else:
-                    print("filter_framework: new_track not ported/created")
-                rp.static_plotTrackMan_initialization(lst_det_per_loop_cycle_LR,
-                                                      lst_not_assigned_LR,
-                                                      new_track_LR)
-            else:
-                print('filter_framework: There is no detection for current LR mcc ',i)
-
-        i_prev = i + 1
-        # This line is redundant if only one mcc is being processed per loop cycle.
-        # However if mcc_step is different than 1, it might be good to keep it here.
-
-
-#     TODO: graphical representation of the results
 
 if __name__ == "__main__":
-    config_data = dc.parse_CMDLine("./analysis.cnf")
+    config_data = dc.cnf_file_read("./setup.cnf")
     if config_data:
         main(config_data)
