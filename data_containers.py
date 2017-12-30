0498421ff87e9c45ac5e20a241edd9ac52939aae
petrbojda <petr.bojda@email.cz>
2017-12-30 08:34:50 +0100

    selection by iteration finished, iteration through the list with elements selected accordingly

--- data_containers.py
+++ data_containers.py
@@ -1,10 +1,5 @@
-import scipy.io as sio
 import numpy as np
-import numpy.linalg as npla
 import configparser
-import argparse
-import itertools
-import copy
 import csv
 
 class DetectionPoint(object):
@@ -51,27 +46,21 @@ class DetectionPoint(object):
 class DetectionList(list):
     def __init__(self):
         super().__init__()
-        self._y_minmax = (0, 0)
-        self._x_minmax = (0, 0)
-        self._theta_minmax = (0, 0)
-        self._rvel_minmax = (0, 0)
-        self._rho_minmax = (0, 0)
-        self._mcc_minmax = (0, 0)
+        self._y_minmax = [0, 0]
+        self._x_minmax = [0, 0]
+        self._theta_minmax = [0, 0]
+        self._rvel_minmax = [0, 0]
+        self._rho_minmax = [0, 0]
+        self._mcc_minmax = [0, 0]
 
-        self._y_minmax_iter = (0, 0)
-        self._x_minmax_iter = (0, 0)
-        self._theta_minmax_iter = (0, 0)
-        self._rvel_minmax_iter = (0, 0)
-        self._rho_minmax_iter = (0, 0)
-        self._mcc_minmax_iter = (0, 0)
+        self._y_minmax_iter = [0, 0]
+        self._x_minmax_iter = [0, 0]
+        self._theta_minmax_iter = [0, 0]
+        self._rvel_minmax_iter = [0, 0]
+        self._rho_minmax_iter = [0, 0]
+        self._mcc_minmax_iter = [0, 0]
 
         self._count = 0
-        # self._sel = self.copy()
-        #
-        # print("dc.__init__: identity of lists self and self._sel:", self is self._sel)
-        # print("dc.__init__: equality of lists self and self._sel:", self == self._sel)
-        # print("dc.__init__: type of self:", type(self), "length:", len(self))
-        # print("dc.__init__: type of self._sel:", type(self._sel), "length:", len(self._sel))
 
     def __iter__(self):
         self._count = 0
@@ -83,55 +72,27 @@ class DetectionList(list):
                 raise StopIteration
             else:
                 self._count += 1
-                if  (self._mcc_minmax_iter[0] <= self[self._count-1]._mcc <= self._mcc_minmax_iter[1] and
-                     self._x_minmax_iter[0] <= self[self._count-1]._x <= self._x_minmax_iter[1] and
-                     self._y_minmax[0] <= self[self._count-1]._y <= self._y_minmax[1] and
-                     self._rho_minmax_iter[0] <= self[self._count-1]._rho <= self._rho_minmax_iter[1] and
-                     self._theta_minmax_iter[0] <= self[self._count-1]._theta <= self._theta_minmax_iter[1] and
-                     self._rvel_minmax_iter[0] <= self[self._count-1]._rvel <= self._rvel_minmax_iter[1]):
-                    return self[self._count-1]
-                # TODO: Constrains must be set properly before it starts iterating
-                else:
-                    pass
+                while not(self.test_detection_to_select(self[self._count-1])):
+                    self._count += 1
+                return self[self._count-1]
         else:
             raise StopIteration
 
+    def test_detection_to_select(self,detection):
+        return (self._mcc_minmax_iter[0] <= detection._mcc <= self._mcc_minmax_iter[1] and
+                self._x_minmax_iter[0] <= detection._x <= self._x_minmax_iter[1] and
+                self._y_minmax[0] <= detection._y <= self._y_minmax[1] and
+                self._rho_minmax_iter[0] <= detection._rho <= self._rho_minmax_iter[1] and
+                self._theta_minmax_iter[0] <= detection._theta <= self._theta_minmax_iter[1] and
+                self._rvel_minmax_iter[0] <= detection._rvel <= self._rvel_minmax_iter[1])
+
     def modify_iteration(self,selection):
-        # TODO: Restart minmax constrains when this function is being called repeatedly
         self._mcc_minmax_iter = selection['mcc_tp'] if selection['mcc_tp'] else self._mcc_minmax
         self._x_minmax_iter = selection['x_tp'] if selection['x_tp'] else self._x_minmax
         self._y_minmax_iter = selection['y_tp'] if selection['y_tp'] else self._y_minmax
         self._rho_minmax_iter = selection['rho_tp'] if selection['rho_tp'] else self._rho_minmax
         self._theta_minmax_iter = selection['theta_tp'] if selection['theta_tp'] else self._theta_minmax
         self._rvel_minmax_iter = selection['rvel_tp'] if selection['rvel_tp'] else self._rvel_minmax
-
-        print("dc: modify_iteration called, minmax constrains: ")
-        print("dc: mcc", self._mcc_minmax)
-        print("dc: x", self._x_minmax)
-        print("dc: y", self._y_minmax)
-        print("dc: rho", self._rho_minmax)
-        print("dc: theta", self._theta_minmax)
-        print("dc: rvel", self._rvel_minmax)
-
-        print("dc: modify_iteration called, minmax ITER constrains: ")
-        print("dc: mcc", self._mcc_minmax_iter)
-        print("dc: x", self._x_minmax_iter)
-        print("dc: y", self._y_minmax_iter)
-        print("dc: rho", self._rho_minmax_iter)
-        print("dc: theta", self._theta_minmax_iter)
-        print("dc: rvel", self._rvel_minmax_iter)
-
-       # selected_list = [elem for elem in self if (self._mcc_minmax_iter[0] <= elem._mcc <= self._mcc_minmax_iter[1] and
-       #                                         self._x_minmax_iter[0] <= elem._x <= self._x_minmax_iter[1] and
-       #                                         self._y_minmax[0] <= elem._y <= self._y_minmax[1] and
-       #                                         self._rho_minmax_iter[0] <= elem._rho <= self._rho_minmax_iter[1] and
-       #                                         self._theta_minmax_iter[0] <= elem._theta <= self._theta_minmax_iter[1] and
-       #                                         self._rvel_minmax_iter[0] <= elem._rvel <= self._rvel_minmax_iter[1])]
-
-        # print("dc.modify_iteration: identity of lists self and self._sel:", self is self._sel)
-        # print("dc.modify_iteration: equality of lists self and self._sel:", self == self._sel)
-        # print("dc.modify_iteration: type of self:", type(self), "length:",len(self))
-        # print("dc.modify_iteration: type of self._sel:", type(self._sel), "length:",len(self._sel))
 
     def get_array_mcc(self):
         return [elem._mcc for elem in self]
@@ -164,22 +125,6 @@ class DetectionList(list):
         self._rvel_minmax = (min([elem._rvel for elem in self]), max([elem._rvel for elem in self]))
         self._rho_minmax = (min([elem._rho for elem in self]), max([elem._rho for elem in self]))
         self._mcc_minmax = (min([elem._mcc for elem in self]), max([elem._mcc for elem in self]))
-        print("dc: recalculate_minmax called, minmax constrains: ")
-        print("dc: mcc", self._mcc_minmax)
-        print("dc: x", self._x_minmax)
-        print("dc: y", self._y_minmax)
-        print("dc: rho", self._rho_minmax)
-        print("dc: theta", self._theta_minmax)
-        print("dc: rvel", self._rvel_minmax)
-
-        print("dc: recalculate_minmax called, minmax ITER constrains: ")
-        print("dc: mcc", self._mcc_minmax_iter)
-        print("dc: x", self._x_minmax_iter)
-        print("dc: y", self._y_minmax_iter)
-        print("dc: rho", self._rho_minmax_iter)
-        print("dc: theta", self._theta_minmax_iter)
-        print("dc: rvel", self._rvel_minmax_iter)
-
 
     def append_data_from_csv(self,filename):
         with open(filename,newline='') as csvfile:
@@ -188,10 +133,71 @@ class DetectionList(list):
                 self.append_detection(DetectionPoint(mcc=int(row['mcc'])))
                 self[-1].assign_XYvel(x=float(row['x']), y=float(row['y']), rvel=float(row['vel']))
                 self[-1].complete_rhotheta_from_cartesian()
-                print("dc.append_data_from_csv: length of the list:",len(self))
-                print("dc.append_data_from_csv: last assigned detection:", self[-1]._x,self[-1]._y)
-        print("dc.append_data_from_csv, mcc:", [elem._mcc for elem in self])
-        self.recalculate_minmax()
+                if len(self)==1:
+                    self.assign_minmax(self[-1])
+                    self.assign_minmax_iter(self[-1])
+                else:
+                    self.calculate_minmax(self[-1])
+                    self.calculate_minmax_iter(self[-1])
+
+    def calculate_minmax(self,detection):
+        self._mcc_minmax[0] = detection._mcc if detection._mcc < self._mcc_minmax[0] else self._mcc_minmax[0]
+        self._mcc_minmax[1] = detection._mcc if detection._mcc > self._mcc_minmax[1] else self._mcc_minmax[1]
+        self._x_minmax[0] = detection._x if detection._x < self._x_minmax[0] else self._x_minmax[0]
+        self._x_minmax[1] = detection._x if detection._x > self._x_minmax[1] else self._x_minmax[1]
+        self._y_minmax[0] = detection._y if detection._y < self._y_minmax[0] else self._y_minmax[0]
+        self._y_minmax[1] = detection._y if detection._y > self._y_minmax[1] else self._y_minmax[1]
+        self._rho_minmax[0] = detection._rho if detection._rho < self._rho_minmax[0] else self._rho_minmax[0]
+        self._rho_minmax[1] = detection._rho if detection._rho > self._rho_minmax[1] else self._rho_minmax[1]
+        self._theta_minmax[0] = detection._theta if detection._theta < self._theta_minmax[0] else self._theta_minmax[0]
+        self._theta_minmax[1] = detection._theta if detection._theta > self._theta_minmax[1] else self._theta_minmax[1]
+        self._rvel_minmax[0] = detection._rvel if detection._rvel < self._rvel_minmax[0] else self._rvel_minmax[0]
+        self._rvel_minmax[1] = detection._rvel if detection._rvel > self._rvel_minmax[1] else self._rvel_minmax[1]
+
+    def calculate_minmax_iter(self,detection):
+        self._mcc_minmax_iter[0] = detection._mcc if detection._mcc < self._mcc_minmax_iter[0] else self._mcc_minmax_iter[0]
+        self._mcc_minmax_iter[1] = detection._mcc if detection._mcc > self._mcc_minmax_iter[1] else self._mcc_minmax_iter[1]
+        self._x_minmax_iter[0] = detection._x if detection._x < self._x_minmax_iter[0] else self._x_minmax_iter[0]
+        self._x_minmax_iter[1] = detection._x if detection._x > self._x_minmax_iter[1] else self._x_minmax_iter[1]
+        self._y_minmax_iter[0] = detection._y if detection._y < self._y_minmax_iter[0] else self._y_minmax_iter[0]
+        self._y_minmax_iter[1] = detection._y if detection._y > self._y_minmax_iter[1] else self._y_minmax_iter[1]
+        self._rho_minmax_iter[0] = detection._rho if detection._rho < self._rho_minmax_iter[0] else self._rho_minmax_iter[0]
+        self._rho_minmax_iter[1] = detection._rho if detection._rho > self._rho_minmax_iter[1] else self._rho_minmax_iter[1]
+        self._theta_minmax_iter[0] = detection._theta if detection._theta < self._theta_minmax_iter[0] else self._theta_minmax_iter[0]
+        self._theta_minmax_iter[1] = detection._theta if detection._theta > self._theta_minmax_iter[1] else self._theta_minmax_iter[1]
+        self._rvel_minmax_iter[0] = detection._rvel if detection._rvel < self._rvel_minmax_iter[0] else self._rvel_minmax_iter[0]
+        self._rvel_minmax_iter[1] = detection._rvel if detection._rvel > self._rvel_minmax_iter[1] else self._rvel_minmax_iter[1]
+
+    def assign_minmax(self,detection):
+        self._mcc_minmax[0] = detection._mcc
+        self._mcc_minmax[1] = detection._mcc
+        self._x_minmax[0] = detection._x
+        self._x_minmax[1] = detection._x
+        self._y_minmax[0] = detection._y
+        self._y_minmax[1] = detection._y
+        self._rho_minmax[0] = detection._rho
+        self._rho_minmax[1] = detection._rho
+        self._theta_minmax[0] = detection._theta
+        self._theta_minmax[1] = detection._theta
+        self._rvel_minmax[0] = detection._rvel
+        self._rvel_minmax[1] = detection._rvel
+
+    def assign_minmax_iter(self,detection):
+        self._mcc_minmax_iter[0] = detection._mcc
+        self._mcc_minmax_iter[1] = detection._mcc
+        self._x_minmax_iter[0] = detection._x
+        self._x_minmax_iter[1] = detection._x
+        self._y_minmax_iter[0] = detection._y
+        self._y_minmax_iter[1] = detection._y
+        self._rho_minmax_iter[0] = detection._rho
+        self._rho_minmax_iter[1] = detection._rho
+        self._theta_minmax_iter[0] = detection._theta
+        self._theta_minmax_iter[1] = detection._theta
+        self._rvel_minmax_iter[0] = detection._rvel
+        self._rvel_minmax_iter[1] = detection._rvel
+
+
+
 
     def get_min_mcc(self):
         return self._mcc_minmax[0]
